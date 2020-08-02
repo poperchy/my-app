1229f72ebc385f732cd1dc322513e918e7e9192d
dik <makkarevi4@gmail.com>
2020-08-03 01:38:38 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header";
 import Nav from "./components/Nav";
-import Main from "./components/Main";
 import News from "./components/News";
 import Music from "./components/Music";
 import Settings from "./components/Settings";
@@ -10,24 +8,27 @@ import Friends from "./components/Friends";
 import {Route} from "react-router-dom";
 import DialogsContainer from "./components/DialogsContainer";
 import UsersContainer from "./components/UsersContainer";
+import MainContainer from "./components/MainContainer";
+import HeaderContainer from "./components/HeaderContainer";
+import LoginPage from "./components/login";
 
 
 const App = (props) => {
 
     return (
         <div>
-            <Header/>
+            <HeaderContainer/>
             <div className='App__container'>
                 <Nav/>
                 <div className='App__container-main'>
-                    <Route path='/profile' render={() => <Main/>}/>
+                    <Route path='/profile/:userId?' render={() => <MainContainer/>}/>
                     <Route path='/message' render={() => <DialogsContainer/>}/>
                     <Route path='/friends' render={() => <Friends />}/>
                     <Route path='/users' render={() => <UsersContainer/>}/>
                     <Route path='/news' render={() => <News/>}/>
                     <Route path='/music' render={() => <Music/>}/>
                     <Route path='/settings' render={() => <Settings/>}/>
-
+                    <Route path='/login' render={() => <LoginPage/>}/>
                 </div>
             </div>
         </div>
